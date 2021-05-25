e2d9e955bd1c1257c3cc11bb0a813acff8649ca4
Silvio Molina <molinahuel44@gmail.com>
2021-05-25 16:23:40 -0300

    first

---
+++ djangoherokuapp/settings.py
@@ -0,0 +1,91 @@
+from pathlib import Path
+
+import os
+import dj_database_url
+
+prod_db = dj_database_url.config(conn_max_age=500)
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'l9$xm5n*mvx)7n++c_3ee$-h3=ljvdweqvch_0q0kck(+9b+m+'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'herokuapp'
+]
+
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'djangoherokuapp.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'djangoherokuapp.wsgi.application'
+
+DATABASES['default'].update(prod_db)
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT,'static')
+    )
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFileStorage'
