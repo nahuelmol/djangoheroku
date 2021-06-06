61dcc50afff3054438c7f5d3a81377939136b7a2
Silvio Molina <molinahuel44@gmail.com>
2021-06-06 01:19:30 -0300

    make it better

--- djangoherokuapp/settings.py
+++ djangoherokuapp/settings.py
@@ -85,7 +85,5 @@ PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT,'static')
-    )
+STATICFILES_DIRS = os.path.join(PROJECT_ROOT,'static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFileStorage'
